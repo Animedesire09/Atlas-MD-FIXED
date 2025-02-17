b4718f5035a17dc7a7e7ecb51135d1f16bfa5d2d
Animedesire09 <rajibmahtorakiya2265@gmail.com>
2025-02-17 12:49:11 +0530

    Update Configurations.js

--- Configurations.js
+++ Configurations.js
@@ -9,7 +9,7 @@ if (!gg) {
 global.owner = gg.split(",");
 global.mongodb = process.env.MONGODB || "mongodb+srv://rajibmahtorakiya1539:<db_password>@cluster0.fa3dn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 global.sessionId = process.env.SESSION_ID || "ok";
-global.prefa = process.env.PREFIX || "-";
+global.prefa = process.env.PREFIX || "!";
 global.tenorApiKey = process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
 global.packname = process.env.PACKNAME || `Atlas Bot`;
 global.author = process.env.AUTHOR || "by: Team Atlas";
