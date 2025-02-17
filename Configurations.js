491913eea24ce545087c8b75794d0ebf4f877f80
Animedesire09 <rajibmahtorakiya2265@gmail.com>
2025-02-17 17:00:22 +0530

    Update Configurations.js

--- Configurations.js
+++ Configurations.js
@@ -7,7 +7,7 @@ if (!gg) {
 
 
 global.owner = gg.split(",");
-global.mongodb = process.env.MONGODB || "mongodb+srv://rajibmahtorakiya1539:<db_password>@cluster0.fa3dn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+global.mongodb = process.env.MONGODB || "mongodb+srv://rajibmahtorakiya1539:<1DER1539A>@cluster0.fa3dn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 global.sessionId = process.env.SESSION_ID || "ok";
 global.prefa = process.env.PREFIX || "!";
 global.tenorApiKey = process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
